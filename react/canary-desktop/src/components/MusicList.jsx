dfdc8b5980694fcafb991a6dbfa56de2825c6e59
Salies <serezane@protonmail.com>
2020-03-28 03:14:44 -0300

    partial work on filters. still broken.

--- react/canary-desktop/src/components/MusicList.jsx
+++ react/canary-desktop/src/components/MusicList.jsx
@@ -4,17 +4,30 @@ import {
   } from "react-router-dom";
 import '../styles/MusicList.css'
 
+/*const modes = [
+    "artist", //SEPARATED date, crescente, decrescent --- alphabetical, normal, reversed
+    "date", //ALL crescent, decrescent
+    "alphabetical" //ALL normal, reversed
+];*/
+
+const modes = ["Artist", "None"]; // Filter by:
+
 function MusicList({libObject}) {
+    const [mode, setMode] = useState("artist");
     const [data, setData] = useState(false);
+    const [selectedArtist, setsa] = useState("Ayreon");
+
+    console.log(selectedArtist);
 
     //maybe TODO: optimze, make so it doesn't reload everytime
     /*eslint-disable */
     useEffect(()=>{
-        if(!data){
+        if(!data.albums){
             console.log("Dados em branco, carregando...");
             libObject.libClass.loadLib().then((res)=>{
                 let loadedAlbums = [];
                 let albums = {};
+                let loadedArtists = [];
         
                 res.forEach(function(song, index){
                     if(loadedAlbums.includes(song.albumid)){
@@ -28,13 +41,28 @@ function MusicList({libObject}) {
                         artist:"",
                         date:""
                     };
+
+                    if(song.artist){
+                        albums[song.albumid]["artist"] = song.artist[0]
+                    }
+                    else{
+                        albums[song.albumid]["artist"] = "Undefined Artist"
+                    }
+
+                    if(mode == "artist" && !loadedArtists.includes(albums[song.albumid]["artist"])){
+                        loadedArtists.push(albums[song.albumid]["artist"]);
+                    }
+
+                    if(selectedArtist && albums[song.albumid]["artist"] != selectedArtist){
+                        delete albums[song.albumid];
+                        loadedAlbums.splice(loadedAlbums.indexOf(song.albumid));
+                        return;
+                    }
         
                     if(song.album){
                         albums[song.albumid]["name"] = song.album
                     }
-                    if(song.artist){
-                        albums[song.albumid]["artist"] = song.artist[0]
-                    }
+
                     if(song.date){
                         albums[song.albumid]["date"] = song.date[0]
                     }
@@ -43,22 +71,58 @@ function MusicList({libObject}) {
                     albums[song.albumid]["tracks"].push({...song, index});
                 });
 
+                console.log(albums);
+
                 const a = Object.keys(albums).map(key => 
                     <Link key={key} to={{ pathname: `/album/${key}`, state: { data: albums[key]} }}>
                     <div style={{backgroundImage:`url('${process.env.PUBLIC_URL}/cache/covers/${key}.jpg')`}}></div>
                     </Link>
                 );
 
-                setData(a);
+                let l = false;
+
+                if(loadedArtists.length){
+                    let lm = loadedArtists.map(artist => <span onClick={()=>{console.log(artist); setsa(artist);}}>{artist}</span>);
+                    l = <div className="artist-list">{lm}</div>
+                    //{data.artists}
+                }
+
+                setData({albums:a, artists:l});
             });
         }
-    }, [data]);
+    }, [data, selectedArtist]);
     /*eslint-enable */
 
     return (
-      <div className="music-list albumlist">
-          {data}
-      </div>
+        <div>
+            {data.artists}
+            <div>
+                Filter by:
+                <select value={mode} onChange={(e)=> setMode(e.target.value)} name="filter">
+                    <option value="artist">Artist</option>
+                    <option value="none">None</option>
+                </select>
+
+                Sort by (date):
+                <select name="date">
+                    <option value="valor1">Date (crescent)</option>
+                    <option value="valor2">Date (decrescent)</option>
+                    <option value="valor3">Name (A-Z)</option>
+                    <option value="valor4">Name (Z-A)</option>
+                    <option value="valor5">None</option>
+                </select>
+
+                Artist - Sort by (A-Z):
+                <select name="date">
+                    <option value="valor1">A-Z</option>
+                    <option value="valor2">Z-A</option>
+                    <option value="valor3">None</option>
+                </select>
+            </div>
+            <div className="music-list albumlist">
+                {data.albums}
+            </div>
+        </div>
     );
   }
   
